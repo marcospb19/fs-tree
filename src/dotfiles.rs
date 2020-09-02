6553a5e060130031214bdb4d7947a216c0430b4a
João Marcos <marcospb19@hotmail.com>
2020-09-02 02:37:09 -0300

    Started lib.rs with mod dotfiles

---
+++ src/dotfiles.rs
@@ -0,0 +1,13 @@
+use std::path::Path;
+
+#[derive(Debug)]
+pub struct DotfileGroup {
+    // starting_path: PathBuf,
+// files: Vec<File>,
+}
+
+impl DotfileGroup {
+    pub fn new<T: AsRef<Path>>(_path: T) -> Self {
+        DotfileGroup {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod cli;
 
+use crate::dotfiles::DotfileGroup;
+
 fn main() {
     let args = cli::parse_args();
-    println!("{:?}", args.value_of("ARG").unwrap());
+    let arg = args.value_of("ARG").unwrap();
+    let group = DotfileGroup::new(arg);
+    println!("{:#?}", group);
 }
