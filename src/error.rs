f7bbbc19402e453f9809546e2227f85eb5011068
João Marcos <marcospb19@hotmail.com>
2020-09-03 16:14:48 -0300

    Basic implementation of DotaoError completed
    Added messages for each variant in Display
    
    ReadError wraps io::Error and a path for referring to the error location
    
    NotFoundInFilesystem for when function calls point to nothing
    
    NotADirectory for when function requires a directory

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,21 @@
-use std::{error, fmt, io, result};
+use std::{error, fmt, io, path::PathBuf, result};
 
 pub type Result<T> = result::Result<T, DotaoError>;
 
 /// DotaoError covers all possible errors from this library
 #[derive(Debug)]
 pub enum DotaoError {
-    ReadError { source: io::Error },
-    IoError(io::Error),
-    Other,
+    ReadError { path: PathBuf, source: io::Error },
+    NotFoundInFilesystem,
+    NotADirectory,
 }
 
 impl error::Error for DotaoError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
-            DotaoError::ReadError { source } => Some(source),
-            DotaoError::IoError(_) => None,
-            DotaoError::Other => None,
+            DotaoError::ReadError { source, .. } => Some(source),
+            DotaoError::NotFoundInFilesystem => None,
+            DotaoError::NotADirectory => None,
         }
     }
 }
@@ -23,15 +23,9 @@ impl error::Error for DotaoError {
 impl fmt::Display for DotaoError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            DotaoError::ReadError { .. } => write!(f, "temporary error_message"),
-            DotaoError::IoError(_) => write!(f, "temporary error_message"),
-            DotaoError::Other => write!(f, "temporary error_message"),
+            DotaoError::ReadError { source, .. } => source.fmt(f),
+            DotaoError::NotFoundInFilesystem => write!(f, "File not found"),
+            DotaoError::NotADirectory => write!(f, "File is not a directory"),
         }
-    }
-}
-
-impl From<io::Error> for DotaoError {
-    fn from(err: io::Error) -> Self {
-        DotaoError::IoError(err)
     }
 }
