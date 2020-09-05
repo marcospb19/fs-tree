7ddc06297e90c1118cf23243875cf754eb2c3502
João Marcos <marcospb19@hotmail.com>
2020-09-04 23:54:13 -0300

    Clap added main argment GROUPS

--- src/bin/dotao/cli.rs
+++ src/bin/dotao/cli.rs
@@ -6,12 +6,17 @@ pub(super) fn parse_args() -> clap::ArgMatches<'static> {
     version.push_str("\nhttps://github.com/marcospb19/dotao");
 
     App::new(crate_name!())
-        .arg(Arg::with_name("ARG")) // Temporary
         .settings(&[ColoredHelp, ArgRequiredElseHelp])
         .version(version.as_ref())
         .about("See --help for more detailed help.")
         .long_about("See -h for shorter help.")
         .help_message("Display help information.")
         .version_message("Display version information.")
+        .arg(
+            Arg::with_name("GROUPS")
+                .multiple(true)
+                .required(true)
+                .help("List of dotfile groups that will be linked the HOME directory."),
+        )
         .get_matches()
 }
