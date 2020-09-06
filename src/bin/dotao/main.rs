0bededdd6c500d589e735e194e74d15a2795317e
João Marcos <marcospb19@hotmail.com>
2020-09-06 11:07:50 -0300

    New main with reworked CLI, DotfileGroup pending
    Current Progress:
    main.rs fills a Vec using DotfileGroup::from_path, but it's incomplete
    Until now, I spent most of the time preparing the code necessary in
    File and DotaoError, so now we can start our work in DotfileGroup

--- src/bin/dotao/main.rs
+++ src/bin/dotao/main.rs
@@ -1,10 +1,41 @@
 mod cli;
 
-use dotao::dotfiles::DotfileGroup;
+use dotao::{dotfiles::DotfileGroup, error::*};
+
+use std::process;
 
 fn main() {
     let args = cli::parse_args();
-    let arg = args.value_of("ARG").unwrap();
-    let group = DotfileGroup::new(arg);
-    println!("{:#?}", group);
+
+    let mut groups: Vec<DotfileGroup> = vec![];
+    let mut error_occurred = false;
+
+    // For each arg of GROUPS
+    for group_path in args.values_of("GROUPS").unwrap() {
+        // Try to transform into DotfileGroup
+        let group: Result<DotfileGroup> = DotfileGroup::from_directory_path(group_path);
+
+        if let Ok(group) = group {
+            groups.push(group);
+        } else if let Err(err) = group {
+            error_occurred = true;
+            // Display customized error message
+            match err {
+                DotaoError::ReadError { path, source } => {
+                    eprintln!(
+                        "Error: Read error for group '{}': {}: '{}'.",
+                        group_path,
+                        source,
+                        path.display()
+                    );
+                },
+                other_err => eprintln!("Error: {}: '{}'", other_err, group_path),
+            }
+        }
+    }
+
+    if error_occurred {
+        process::exit(1);
+    }
+    println!("{:#?}", groups);
 }
--- src/dotfiles.rs
+++ src/dotfiles.rs
@@ -1,14 +1,36 @@
-use crate::error::*;
+use crate::{
+    error::*,
+    file::{File, FlatFileType},
+};
+
 use std::path::{Path, PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct DotfileGroup {
-    // starting_path: PathBuf, files: Vec<File>,
+    pub starting_path: PathBuf,
+    pub files: Vec<File>,
 }
 
 impl DotfileGroup {
-    pub fn new<T: AsRef<Path>>(_path: T) -> Result<Self> {
-        let _a = PathBuf::new().metadata()?;
-        Ok(DotfileGroup {})
+    pub fn new(path: PathBuf, files: Vec<File>) -> Self {
+        DotfileGroup {
+            starting_path: path,
+            files,
+        }
+    }
+
+    pub fn from_directory_path(path: impl AsRef<Path>) -> Result<Self> {
+        if !path.as_ref().exists() {
+            return Err(DotaoError::NotFoundInFilesystem);
+        }
+
+        match FlatFileType::from_path(path)? {
+            FlatFileType::Directory => {},
+            _ => return Err(DotaoError::NotADirectory),
+        }
+
+        unimplemented!();
+
+        Ok(DotfileGroup::default())
     }
 }
