10a238f9496e7df7f9eaf59e7499240c97c06895
João Marcos <marcospb19@hotmail.com>
2020-09-12 08:04:55 -0300

    Reverting some work in error.rs

--- src/error.rs
+++ src/error.rs
@@ -6,10 +6,8 @@ pub type Result<T> = result::Result<T, DotaoError>;
 #[derive(Debug)]
 pub enum DotaoError {
     ReadError { path: PathBuf, source: io::Error },
-    UnableToEnterDirectory { path: PathBuf, source: io::Error },
     NotFoundInFilesystem,
     NotADirectory,
-    LinkError(DotaoLinkError),
 }
 
 use DotaoError::*;
@@ -17,8 +15,7 @@ use DotaoError::*;
 impl error::Error for DotaoError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
-            ReadError { source, .. } | UnableToEnterDirectory { source, .. } => Some(source),
-            LinkError(source) => Some(source),
+            ReadError { source, .. } => Some(source),
             _ => None,
         }
     }
@@ -28,36 +25,8 @@ impl fmt::Display for DotaoError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             ReadError { source, .. } => source.fmt(f),
-            UnableToEnterDirectory { .. } => write!(f, "Unable to enter directory"),
             NotFoundInFilesystem => write!(f, "File not found"),
             NotADirectory => write!(f, "File is not a directory"),
-            LinkError(source) => {
-                write!(f, "Link error: ")?;
-                source.fmt(f)
-            },
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum DotaoLinkError {
-    A,
-}
-
-use DotaoLinkError::*;
-
-impl error::Error for DotaoLinkError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match self {
-            A => None,
-        }
-    }
-}
-
-impl fmt::Display for DotaoLinkError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            A => write!(f, ""),
         }
     }
 }
