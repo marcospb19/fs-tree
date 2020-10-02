53f12b1ae8d25ce89b7c33d082fc9c36b044b07d
João M. Bezerra <marcospb19@hotmail.com>
2020-10-02 02:45:43 -0300

    Removing lib.rs code
    It'll be moved into tree_file.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,7 @@
 pub mod lexer;
 pub mod parser;
 
-#[derive(Debug)]
-pub struct Value {}
-
-#[derive(Debug)]
-pub struct Group {
-    pub name: String,
-    pub values: Vec<Value>,
-}
-
-#[derive(Debug)]
-pub struct Groups {
-    pub underlying_vec: Vec<Group>,
-}
-
-// impl IntoIter for Groups {}
-
-pub fn get_groups(text: impl AsRef<str>) -> Vec<Group> {
-    let _text = text.as_ref();
-    vec![]
-}
+//
+mod tree_file;
+//
+pub use tree_file::*;
