40696e38c16b104425ebff3426074da04b6e1771
João M. Bezerra <marcospb19@hotmail.com>
2020-10-24 19:03:08 -0300

    Functional lexer done

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ RESTARTING!
 - [x] FileStructure crate implementation for file structure representation (other project link goes here)
 - [x] API decisions
 - [x] Syntax stabilization + examples skratch
-- [ ] Functional lexer
+- [x] Functional lexer
 - [ ] Functional parser
 - [ ] Error message pointing to line and column
+- [ ] Fix lexer regex support to escaped and special characters
 - [ ] Implement groups
 - [ ] Implement flags support
 - [ ] Improve this readme
@@ -19,6 +20,7 @@ RESTARTING!
 - [ ] Enum for types of flags (normal, parentInherited, and groupInherited)
 - [ ] Nicer error messages, that say what was expected and where
 - [ ] Documentation, lol
+- [ ] Review where we accept whitespaces
 - [ ] Rethink decision about non-UTF-8, or document it better (Documentation, lol)
 - [ ] Document FileStructure too if you're using it as an dependency
 - [ ] What about writing good docs?
--- src/lexer.rs
+++ src/lexer.rs
@@ -2,18 +2,25 @@ use logos::Logos;
 
 #[rustfmt::skip]
 #[derive(Logos, Debug, PartialEq)]
-pub enum LogosToken {
-    // // Groups
-    // // Examples:
-    // //   - [group_name]
-    // //   -[spacing_is_optional]
-    // #[regex(r"- *\[\[([^\]\\]|\\t|\\u|\\\])*\]\]")]
-    // Group(String),
+pub enum Token {
+    // Groups
+    // Examples:
+    //   - [group_name]
+    //   -[spacing_is_optional]
+    #[regex(" *- *\\[ *[^\\] ]+ *\\]", |lex| {
+        let start = lex.slice().find("[").unwrap();
+        let end = lex.slice().rfind("]").unwrap();
 
-    // // Flags, escaped like strings, but use () instead of ""
-    // // Examples:
-    // //   (a, b)
-    // //   (unix)
+        let slice: &str = &lex.slice()[start + 1..end].trim();
+
+        String::from(slice)
+    })]
+    Group(String),
+
+    // Flags, escaped like strings, but use () instead of ""
+    // Examples:
+    //   (a, b)
+    //   (unix)
     // #[regex(r"\(([^\)\\]|\\t|\\u|\\n|\\\))*\)", |lex| {
     //     let vec = vec![];
     //     let potential_flags =
@@ -23,24 +30,35 @@ pub enum LogosToken {
     // })]
     // Flags(Vec<String>),
 
-    // // Value token delimited by ""
-    // #[regex(r#""([^"\\]|\\t|\\u|\\n|\\")*""#)]
-    // Value(String),
+    // Value token delimited by ""
+    #[regex("\"[^\"]+\"", |lex| {
+        let start = lex.slice().find("\"").unwrap();
+        let end = lex.slice().rfind("\"").unwrap();
 
-    // #[regex(":")]
-    // DoubleDots,
+        let slice: &str = &lex.slice()[start + 1..end];
 
-    // #[regex(r"\[")]
-    // OpenBracket,
+        String::from(slice)
+    })]
+    Value(String),
 
-    // #[regex("]")]
-    // CloseBracket,
+    #[token(":")]
+    DoubleDots,
 
-    // #[regex("->")]
-    // SymlinkArrow,
+    #[token("[")]
+    OpenBracket,
 
-    // #[token(r"(,|\\n)")]
-    // Separator,
+    #[token("]")]
+    CloseBracket,
+
+    #[token("->")]
+    SymlinkArrow,
+
+    // New line or comma
+    #[regex("(\\n|,)")]
+    Separator,
+
+    // Ignore whitespace
+    #[regex(" \\t", logos::skip)]
 
     // // // Ignore whitespace
     // #[regex(r"[ \t\n\f]+", logos::skip)]
@@ -49,4 +67,31 @@ pub enum LogosToken {
     // // Anything unexpected
     #[error]
     Error,
+}
+
+#[cfg(test)]
+mod lexer_tests {
+    use super::{
+        Token::{self, *},
+        *,
+    };
+
+    // Test lexer
+    fn tl(text: &str, expected: Token) {
+        let mut lex = Token::lexer(text);
+        assert_eq!(lex.next().unwrap(), expected)
+    }
+
+    #[test]
+    fn group_regex() {
+        tl("- [asd]", Group(String::from("asd")));
+        tl("   -    [asd]", Group(String::from("asd")));
+        tl("   -[ asd  ]", Group(String::from("asd")));
+    }
+
+    #[test]
+    fn separator_regex() {
+        tl("\n", Separator);
+        tl(",", Separator);
+    }
 }
