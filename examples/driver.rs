8395783aa80fdc715d2098c184235d5eab2fb860
João M. Bezerra <marcospb19@hotmail.com>
2020-12-27 14:41:40 -0300

    Update examples

--- examples/driver.rs
+++ examples/driver.rs
@@ -3,10 +3,10 @@ use tree_file::Groups;
 use std::{fs, io};
 
 fn main() -> io::Result<()> {
-    // let path = "examples/multiple_groups.tree";
-    let path = "examples/simple.tree";
     // let path = "examples/simplest.tree";
-    // let path = "examples/dotao.tree";
+    // let path = "examples/simple.tree";
+    // let path = "examples/multiple_groups.tree";
+    let path = "examples/dotao.tree";
     let text = fs::read_to_string(path)?;
 
     let groups = Groups::from_text(&text);
