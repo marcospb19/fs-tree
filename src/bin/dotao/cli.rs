2a1db4008db0a0092d721ff073bd8918b26c0a4f
João M. Bezerra <marcospb19@hotmail.com>
2021-02-19 17:21:56 -0300

    Fix cli.rs color output

--- src/bin/dotao/cli.rs
+++ src/bin/dotao/cli.rs
@@ -1,12 +1,9 @@
 use clap::{crate_name, crate_version, App, AppSettings, Arg, SubCommand};
 
-// Why isn't this working as intended?
-// .about("See --help for more detailed help.")
-// .long_about("See -h for shorter help.")
 pub fn parse_args() -> clap::ArgMatches<'static> {
     App::new(crate_name!())
-        .version(crate_version!())
         .settings(&[AppSettings::ColoredHelp, AppSettings::ArgRequiredElseHelp])
+        .version(crate_version!())
         .help_message("Display help information.")
         .version_message("Display version information.")
         .subcommand(
@@ -33,24 +30,16 @@ pub fn parse_args() -> clap::ArgMatches<'static> {
                 .aliases(&["r", "rm", "delete"])
                 .about("Remove groups from the tree file."),
         )
-        .subcommand(SubCommand::with_name("link")
-                // .arg(
-                //     Arg::with_name("groups")
-                //         .multiple(true)
-                //         .help("Group folders to remove from the tree (default=all)"),
-                // )
-                .alias("l")
-                .about("Link groups in the tree file."))
+        .subcommand(
+            SubCommand::with_name("link")
                 .settings(&[AppSettings::ColoredHelp])
+                .alias("l")
+                .about("Link groups in the tree file."),
+        )
         .subcommand(
             SubCommand::with_name("unlink")
-                // .arg(
-                //     Arg::with_name("groups")
-                //         .multiple(true)
-                //         .help("Group folders to remove from the tree (default=all)"),
-                // )
-                .alias("u")
                 .settings(&[AppSettings::ColoredHelp])
+                .alias("u")
                 .about("Unlink groups in the tree file."),
         )
         .subcommand(
@@ -67,28 +56,3 @@ pub fn parse_args() -> clap::ArgMatches<'static> {
         )
         .get_matches()
 }
-// .arg(
-//     Arg::with_name("interactive_mode")
-//         .short("i")
-//         .help("Run in interactive mode, try to solve conflicts with questions,"),
-// )
-// .arg(
-//     Arg::with_name("overwrite_symlink")
-//         .short("s")
-//         .help("Overwrite symlinks."),
-// )
-// .arg(
-//     Arg::with_name("overwrite_file")
-//         .short("f")
-//         .help("Overwrite files."),
-// )
-// .arg(
-//     Arg::with_name("overwrite_directory")
-//         .short("d")
-//         .help("Overwrite directories."),
-// )
-// .arg(
-//     Arg::with_name("fake-run")
-//         .long("fake")
-//         .help("Don't mess with files."),
-// )
