69f4693f14494e8ba8e01b1c13bbda120d801473
João M. Bezerra <marcospb19@hotmail.com>
2021-02-19 17:23:06 -0300

    Moving dotao bin crate source code
    From src/bin/dotao/src/*.rs to src/*.rs
    And removing unused (old) files

--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-// /// Just `DotfileGroup`, _Dotao_ representation of a dotfile.
-// pub mod dotfiles;
-// /// Inside of this is the `DotaoError` and `Result<T, DotaoError>`
-// pub mod error;
-// /// Trait to link dotfiles, implemented for DotfileGroup
-// pub mod link;
